3f35dfbbe5843d5064bcc891613f6f8e166008a4
brocketdesign <50971180+brocketdesign@users.noreply.github.com>
2023-09-04 13:59:47 +0900

    Update getHighestQualityVideoURL.js
    update youtube video scraper

--- modules/getHighestQualityVideoURL.js
+++ modules/getHighestQualityVideoURL.js
@@ -7,25 +7,12 @@ async function getHighestQualityVideoURL(video_id, user, stream = true) {
   try {
     const userId = user._id;
     
-    const foundElement = await global.db.collection('medias').findOne({_id:new ObjectId(video_id)})
-console.log(foundElement)
+    const videoDocument = await global.db.collection('medias').findOne({_id:new ObjectId(video_id)})
 
-    if(foundElement.filePath){
-      console.log('The element has already been downloaded', foundElement)
-      updateSameElements(foundElement,{isdl:true,isdl_data:new Date()})
-      return foundElement.filePath.replace('public','')
-    }
-
-    if (hasBeenScrapedRecently(foundElement)) {
-      //return getVideoFilePathOrHighestQualityURL(foundElement, stream);
-    }
-
-    if (foundElement.mode == "3") {
-      await saveData(user, foundElement,{filePath:foundElement.url})
-      return foundElement.url; 
-    }
-    if (foundElement.mode == "2" || foundElement.mode == "4") {
-      return foundElement.link; 
+    if(videoDocument.filePath){
+      console.log('The element has already been downloaded', videoDocument)
+      updateSameElements(videoDocument,{isdl:true,isdl_data:new Date()})
+      return videoDocument.filePath.replace('public','')
     }
 
     return await searchVideoYoutube(videoDocument, user, stream) 
@@ -33,26 +20,6 @@ console.log(foundElement)
     console.log('Error occurred while getting the video URL:', error);
     return null;
   }
-}
-
-function hasBeenScrapedRecently(videoDocument) {
-  const currentTime = Date.now();
-  const lastScrapedTime = videoDocument.last_scraped || 0;
-  const timeDifference = currentTime - lastScrapedTime;
-  const result = !!((timeDifference < ONE_DAY_IN_MS) && (videoDocument.highestQualityURL || videoDocument.streamingUrl))
-  return result;
-}
-
-function getVideoFilePathOrHighestQualityURL(videoDocument, stream) {
-  console.log('Video has already been scraped within the last 24 hours. Using cached URL.');
-  if(videoDocument.link.includes('youtube.com')){
-    if(stream){
-      return videoDocument.filePath ? videoDocument.filePath : videoDocument.streamingUrl;
-    }else{
-      return videoDocument.filePath ? videoDocument.filePath : videoDocument.link;
-    }
-  }
-  return videoDocument.filePath ? videoDocument.filePath : videoDocument.highestQualityURL;
 }
 
 async function searchVideoYoutube( videoDocument, user, stream){
