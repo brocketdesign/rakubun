5c0b21072ebf89d4ed80f8d64bc93d9728636b87
brocketdesign <50971180+brocketdesign@users.noreply.github.com>
2024-05-22 14:51:49 +0900

    using official ngrok library

--- services/startNgrok.js
+++ services/startNgrok.js
@@ -1,24 +1,10 @@
-const ngrok = require('ngrok');
-const { exec } = require('child_process');
+const ngrok = require('@ngrok/ngrok');
 
 async function startNgrok(port) {
   try {
-    // Attempt to kill any existing ngrok processes
-    await new Promise((resolve, reject) => {
-      exec('pkill -f ngrok', (error, stdout, stderr) => {
-        if (error) {
-          console.warn('Failed to kill existing ngrok processes:', stderr);
-          resolve(); // Resolve anyway to continue with ngrok setup
-        } else {
-          console.log('Existing ngrok processes killed', stdout);
-          resolve();
-        }
-      });
-    });
-
-    // Ensure any existing NGROK tunnels are disconnected
-    await ngrok.disconnect();
-    await ngrok.kill();
+    // Disconnect any existing tunnels if ngrok is already running
+    await ngrok.disconnect();  // This will only disconnect tunnels without killing the ngrok process
+    await ngrok.kill();        // This ensures that all ngrok processes are terminated
 
     // Set NGROK auth token if required
     if (process.env.NGROK_AUTH_TOKEN) {
@@ -26,11 +12,13 @@ async function startNgrok(port) {
     }
 
     // Start a new NGROK tunnel to the specified port
-    const url = await ngrok.connect({
-      addr: port,
-      region: 'jp' // Optionally set the region
-    });
+    const url = (await ngrok.connect({
+      addr: port,      // Port or address to tunnel to
+      region: 'jp'     // Optionally set the region
+    })).url();
+
     console.log(`NGROK is publicly exposing your localhost at port ${port} on: ${url}`);
+
     return url; // Return the NGROK URL for use in other parts of your application
   } catch (error) {
     console.error('Failed to start NGROK:', error);
